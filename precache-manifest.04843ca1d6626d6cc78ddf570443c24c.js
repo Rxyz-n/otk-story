36e3ca1bdaad375ace7474b7603e20f4e573b5c2
Nano <hikarishido1997@gmail.com>
2020-09-10 12:21:32 +0700

    Updates

---
+++ precache-manifest.04843ca1d6626d6cc78ddf570443c24c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a430ecda959303ede9d5c36c4390df9c",
+    "url": "/otk-story/index.html"
+  },
+  {
+    "revision": "ff3749e177f35fe10c26",
+    "url": "/otk-story/static/js/2.070812ef.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/otk-story/static/js/2.070812ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996fecb61e67c4e2d3ce",
+    "url": "/otk-story/static/js/main.62ebcfcd.chunk.js"
+  },
+  {
+    "revision": "67db3b511c01b26b64da",
+    "url": "/otk-story/static/js/runtime-main.c5c857c1.js"
+  },
+  {
+    "revision": "6ad731b98203bccd67c15fe28cd0edec",
+    "url": "/otk-story/static/media/logo.6ad731b9.png"
+  }
+]);
