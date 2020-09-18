79d603769ff64d76fc38438168fe2e91fdcaead8
Nano <hikarishido1997@gmail.com>
2020-09-18 20:33:18 +0700

    Add GA

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 
 import ThemeProvider from 'context/ThemeContext';
 import StoriesProvider from 'context/StoriesContext';
 import GlobalStyle from 'styles/Global';
 import App from 'App';
+
+ReactGA.initialize('UA-178421575-1');
+ReactGA.pageview('/');
 
 ReactDOM.render(
   <ThemeProvider>
